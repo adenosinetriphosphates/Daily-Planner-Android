77e4d1f86b001bab89129daca1e661a0bb991197
Arnav Jain <indiaarnavj@gmail.com>
2026-03-28 12:41:51 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,9 @@
-const CACHE = 'daily-planner-v1';
+const CACHE = 'daily-planner-v2';
 const ASSETS = [
-  './daily-planner.html',
-  './manifest.json'
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', e => {
@@ -20,6 +22,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./daily-planner.html')))
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
   );
 });
